5b9e85657c5c56b9362cb36420603e34f92eec42
Daniil <dan.kattsyn@gmail.com>
2020-04-22 20:33:31 +0700

    implement HW1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+
+	public static void main(String[] args) {
+		byte coreProcessor = 2;
+		short threadsProcessor = 4;
+		int sizeRAM = 8;
+		long maxSizeSsdInGb = 256;
+		float maxFrequencyCpu = 3.7f;
+		double minFrequencyCpu = 1.6;
+		char markingIntelCpu = 'i';
+		boolean haveHDD = false;
+
+		System.out.println(coreProcessor);
+		System.out.println(threadsProcessor);
+		System.out.println(sizeRAM);
+		System.out.println(maxSizeSsdInGb);
+		System.out.println(maxFrequencyCpu);
+		System.out.println(minFrequencyCpu);
+		System.out.println(markingIntelCpu);
+		System.out.println(haveHDD);
+	}
+
+}
