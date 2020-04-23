5e9fe4f75b701f1e4380bee277409af78a01a0cd
Daniil <dan.kattsyn@gmail.com>
2020-04-23 16:46:46 +0700

    implement HW1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,16 +2,15 @@ public class MyFirstGame {
 
 	public static void main(String[] args) {
 		int randomNumber = 82;
-		int myNumber = 45;
+		int myNumber = 0;
 		do {
 			if (myNumber < randomNumber) {
 				System.out.println("Введеное вами число меньше того, которое загаsдал компьютер");
 			} else if (myNumber > randomNumber) {
 				System.out.println("Введеное вами число больше того, которое загадал компьютер");
-			} else if (myNumber == randomNumber) {
-				 System.out.println("Вы угадали число!");
-				 break;
 			}
+			myNumber++;
 		} while (myNumber != randomNumber);
+		System.out.println("Вы угадали число");
 	}
 }
