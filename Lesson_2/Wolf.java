39ffef9cc3c65eabe6cb53b50f850a5ddd38f1fd
Daniil <dan.kattsyn@gmail.com>
2020-04-24 22:22:54 +0700

    implement HW2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,30 @@
+public class Wolf {
+	
+	char gender;
+	String name;
+	float weight;
+	int age;
+	String color;
+
+	void go() {
+		System.out.println("Вы начали движение!");
+	}
+
+	void sitDown() {
+		System.out.println("Вы только что присели");
+	}
+
+	boolean run() {
+		System.out.println("Режим бега активирован");
+		return true;
+	}
+
+	String howl() {
+		return "Wo-wo";
+	}
+
+	boolean hunting() {
+		System.out.println("Режим охоты включен!");
+		return true;
+	}
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,23 @@
+public class WolfTest {
+	
+	public static void main(String[] args) {
+		Wolf wolf = new Wolf();
+		wolf.gender = 'M';
+		wolf.name = "Borz";
+		wolf.weight = 8.6F;
+		wolf.age = 5;
+		wolf.color = "black";
+
+		System.out.println("Пол - " + wolf.gender);
+		System.out.println("Кличка - " + wolf.name);
+		System.out.println("Вес - " + wolf.weight);
+		System.out.println("Возраст - " + wolf.age);
+		System.out.println("Цвет - " + wolf.color);
+
+		wolf.go();
+		wolf.sitDown();
+		wolf.run();
+		wolf.howl();
+		wolf.hunting();
+	}
+}
