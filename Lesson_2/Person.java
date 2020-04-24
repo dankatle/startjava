e2b3cdc8de271d1adeae51fbdd6303350409100a
Daniil <dan.kattsyn@gmail.com>
2020-04-24 20:02:16 +0700

    implement HW2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	String name = "John";
+	float height = 2.05f;
+	float weight = 80f;
+	char gender = "M";
+
+	void go() {
+		System.out.println("Начал движение");
+	}
+
+	void sitDown() {
+		System.out.println("Я присел!")
+	}
+
+	boolean run() {
+		System.out.println("Бег активирован");
+		return true;
+	}
+
+	String speak() {
+		return "Я умею говорить";
+	}
+
+	void learnJava() {
+		System.out.println("Я приступили к изучению Java");
+	}
+}
